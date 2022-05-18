460c6eb889f60f6a0c6ed7c97fb42de1183e269e
punica24@njfu.edu.cn <mingyan130824@gmail.com>
2022-05-19 00:40:50 +0800

    update chapter2

--- runbookdown.R
+++ runbookdown.R
@@ -2,3 +2,12 @@ library(bookdown)
 bookdown::render_book("index.Rmd")
 browseURL("docs/index.html")
 ?geom_col
+
+specie <- c(rep("sorgho" , 3) , rep("poacee" , 3) , rep("banana" , 3) , rep("triticum" , 3) )
+condition <- rep(c("normal" , "stress" , "Nitrogen") , 4)
+value <- abs(rnorm(12 , 0 , 15))
+data <- data.frame(specie,condition,value)
+
+library(rio)
+export(data,file = "example_data/04-barplot/dat02_grouped_barplot.xlsx")
+
