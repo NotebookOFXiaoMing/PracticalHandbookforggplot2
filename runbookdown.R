2f81e7f5d10adc86d352c480c2916b3337900155
punica24@njfu.edu.cn <mingyan130824@gmail.com>
2022-05-21 16:55:53 +0800

    boxplot

--- runbookdown.R
+++ runbookdown.R
@@ -2,7 +2,7 @@ library(bookdown)
 bookdown::render_book("index.Rmd")
 browseURL("docs/index.html")
 ?geom_col
-
+?read_excel
 specie <- c(rep("sorgho" , 3) , rep("poacee" , 3) , rep("banana" , 3) , rep("triticum" , 3) )
 condition <- rep(c("normal" , "stress" , "Nitrogen") , 4)
 value <- abs(rnorm(12 , 0 , 15))
